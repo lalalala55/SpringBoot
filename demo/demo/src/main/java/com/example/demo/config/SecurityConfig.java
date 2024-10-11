47d2c2bf021e623f65f4d47edbc19532c451ec2c
GOTTIPATI GOPI SAI <gottipatigopisai@gmail.com>
2024-10-11 16:01:15 +0530

    06-bcrypt password encoder

--- demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,9 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,7 +41,7 @@ public class SecurityConfig {
                 Arrays.asList(
                         User.builder()
                                 .username("sita")
-                                .password("s143")
+                                .password(passwordEncoder.encode("ram143")) // encoding the password before
                                 .roles("ADMIN")
                                 .build()
                 )
@@ -46,10 +50,5 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(
             users
         );
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
