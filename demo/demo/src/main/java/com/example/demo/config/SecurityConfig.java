0711c48c14dd5809d6c84b0d28c3c33a42ea8da3
GOTTIPATI GOPI SAI <gottipatigopisai@gmail.com>
2024-10-11 14:39:18 +0530

    05-with withDefaultPasswordEncoder()

--- demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,7 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -16,9 +26,25 @@ public class SecurityConfig {
                         requests.requestMatchers("/", "/demo").permitAll()
                                 .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService(){
 
+        List<UserDetails> users = new ArrayList<>(
+                Arrays.asList(
+                        User.withDefaultPasswordEncoder()
+                                .username("sita")
+                                .password("s143")
+                                .roles("ADMIN")
+                                .build()
+                )
+        );
+
+        return new InMemoryUserDetailsManager(
+            users
+        );
+    }
 }
